dd8967d7c113791440ccc73c2973307807d13ca3
Islom0906 <iabdugofurov99@gmail.com>
2023-04-29 14:48:02 +0500

    object contain

--- src/components/Category-card/Category-card.jsx
+++ src/components/Category-card/Category-card.jsx
@@ -14,7 +14,7 @@ const CategoryCard = ({image, text, isCard,isActive}) => {
                         className={'absolute ml-[120px] mt-24  w-0 h-0 bg-primary rounded-full z-10 group-hover:w-[200%] group-hover:h-[200%] duration-500'}></div>
                     <div
                         className={`absolute bottom-0 -right-5 2xl:w-[85%] xl:w-[95%] w-[90%] 2xl:aspect-[3/5] lg:aspect-[4/5] aspect-[3/3] z-20`}>
-                        <Image src={image?.location} alt={'category image object-contain'}
+                        <Image src={image?.location} alt={'category image '} className={'object-contain'}
                                layout={'fill'}/>
                     </div>
                 </>
